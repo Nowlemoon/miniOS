b6c0c5555d6a4a48c502cf792689d1fa80bb8734
Nowlemoon <143645729+Nowlemoon@users.noreply.github.com>
2023-09-02 15:22:33 +0200

    Main App - Updated

--- main.py
+++ main.py
@@ -1,91 +1,131 @@
 import datetime
 import requests
 import random
+import tkinter as tk
+from geopy.geocoders import Nominatim
 
-def get_weather():
-    # Replace 'API_KEY' with your actual API key
-    api_key = '443356ddc1392c089fd3d4949a22d26b'
-    city = 'Dortmund'  # Replace with the desired city
+def get_weather(latitude, longitude):
+    api_key = '43f6544a97e975225aad17985345b94e'  # Replace with your OpenWeatherMap API key
 
-    url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}'
+    url = f'http://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid={api_key}'
     response = requests.get(url)
     data = response.json()
 
     temperature = data['main']['temp']
     temperature = round(temperature - 273.15, 2)  # Convert temperature from Kelvin to Celsius
 
-    return temperature, city
+    return temperature
 
-def hangman():
+def get_location():
+    geolocator = Nominatim(user_agent="your-app-name")  # Replace "your-app-name" with your app name
+
+    try:
+        location = geolocator.geocode("")
+        latitude = location.latitude
+        longitude = location.longitude
+    except:
+        latitude = 0.0  # Default latitude value
+        longitude = 0.0  # Default longitude value
+
+    return latitude, longitude
+
+def play_hangman():
     words = ['python', 'hangman', 'game', 'programming', 'computer']
     word = random.choice(words)
     guesses = ''
     tries = 6
 
-    while tries > 0:
-        failed = 0
+    def guess_letter():
+        nonlocal word, guesses, tries
+        letter = guess_entry.get().lower()
+        guess_entry.delete(0, tk.END)
 
-        for char in word:
-            if char in guesses:
-                print(char, end=' ')
-            else:
-                print('_', end=' ')
-                failed += 1
+        if not letter.isalpha():
+            return
 
-        if failed == 0:
-            print("\nCongratulations! You guessed the word correctly.")
-            break
+        if letter in guesses:
+            return
 
-        guess = input("\n\nGuess a letter: ")
-        guesses += guess
+        guesses += letter
 
-        if guess not in word:
+        if letter not in word:
             tries -= 1
-            print("Wrong guess!")
-            print(f"You have {tries} tries left.")
 
-        if tries == 0:
-            print("\nSorry, you ran out of tries.")
-            print(f"The word was '{word}'.")
-            break
+        hangman_text.set(get_hangman_text())
 
-def calculator():
-    num1 = float(input("Enter the first number: "))
-    operator = input("Enter the operator (+, -, *, /): ")
-    num2 = float(input("Enter the second number: "))
+        if tries == 0 or all(letter in guesses for letter in word):
+            guess_button.config(state=tk.DISABLED)
 
-    if operator == '+':
-        result = num1 + num2
-    elif operator == '-':
-        result = num1 - num2
-    elif operator == '*':
-        result = num1 * num2
-    elif operator == '/':
-        result = num1 / num2
-    else:
-        print("Invalid operator!")
-        return
+    def get_hangman_text():
+        return ' '.join(letter if letter in guesses else '_' for letter in word)
 
-    print(f"The result is: {result}")
+    hangman_window = tk.Toplevel(root)
+    hangman_window.title("Hangman Game")
 
-def main():
-    while True:
-        command = input("Enter a command (time, weather, hangman, calculator, exit): ")
+    hangman_text = tk.StringVar()
+    hangman_label = tk.Label(hangman_window, textvariable=hangman_text, font=('Arial', 20))
+    hangman_label.pack(pady=10)
 
-        if command == 'time':
-            current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-            print(f"The current time is: {current_time}")
-        elif command == 'weather':
-            temperature, city = get_weather()
-            print(f"The temperature in {city} is {temperature}°C")
-        elif command == 'hangman':
-            hangman()
-        elif command == 'calculator':
-            calculator()
-        elif command == 'exit':
-            break
+    guess_label = tk.Label(hangman_window, text="Guess a letter:")
+    guess_label.pack()
+
+    guess_entry = tk.Entry(hangman_window)
+    guess_entry.pack()
+
+    guess_button = tk.Button(hangman_window, text="Guess", command=guess_letter)
+    guess_button.pack(pady=10)
+
+    def update_hangman_text():
+        hangman_text.set(get_hangman_text())
+        if tries == 0 or all(letter in guesses for letter in word):
+            guess_button.config(state=tk.DISABLED)
         else:
-            print("Invalid command!")
+            guess_button.config(state=tk.NORMAL)
 
-if __name__ == '__main__':
-    main()
+    update_hangman_text()
+
+def exit_app():
+    root.destroy()
+
+def update_time():
+    current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    time_label.config(text=f"Date is: {current_time}")
+    time_label.after(1000, update_time)
+
+def update_weather(latitude, longitude):
+    temperature = get_weather(latitude, longitude)
+    weather_label.config(text=f"The temperature is: {temperature}°C")
+    weather_label.after(60000, lambda: update_weather(latitude, longitude))
+
+root = tk.Tk()
+root.title("Terminal App GUI")
+
+# Mini taskbar at the top
+taskbar_frame = tk.Frame(root, bg='black', height=30)
+taskbar_frame.pack(fill='x')
+
+power_off_button = tk.Button(taskbar_frame, text="Power Off", bg='red', fg='white', command=exit_app)
+power_off_button.pack(side='right')
+
+# Hangman label in the upper-left corner
+hangman_label = tk.Label(root, text="Hangman game")
+hangman_label.pack(anchor='nw', padx=10, pady=10)
+
+# Taskbar at the bottom
+date_frame = tk.Frame(root, bg='black', height=30)
+date_frame.pack(fill='x', side='bottom')
+
+time_label = tk.Label(date_frame, text="Date is: ")
+time_label.pack(side='left')
+
+weather_label = tk.Label(date_frame, text="The temperature is: ")
+weather_label.pack(side='right')
+
+play_hangman_button = tk.Button(root, text="Play Hangman", command=play_hangman)
+play_hangman_button.pack()
+
+latitude, longitude = get_location()
+update_time()
+update_weather(latitude, longitude)
+
+root.mainloop()
